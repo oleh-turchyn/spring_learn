e0c1a1f1a79fcc2e11946d30b2c6c638d723f692
oleh-turchyn <47058088+Leggi19@users.noreply.github.com>
2020-08-19 21:15:02 +0300

    add setter DI

--- src/main/java/com/turchyn/spring/MusicPlayer.java
+++ src/main/java/com/turchyn/spring/MusicPlayer.java
@@ -6,6 +6,11 @@ public class MusicPlayer {
     public MusicPlayer(Music music){
         this.music=music;
     }
+    public MusicPlayer(){}
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
     public void playMusic(){
         System.out.println("Playing: "+music.getSong());
     }
