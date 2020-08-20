b8b934707061889f788b7d8cec0b8ef0bc8ae6f8
oleh-turchyn <47058088+Leggi19@users.noreply.github.com>
2020-08-20 17:44:39 +0300

    added harder DI

--- src/main/java/com/turchyn/spring/MusicPlayer.java
+++ src/main/java/com/turchyn/spring/MusicPlayer.java
@@ -5,16 +5,29 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Component
 public class MusicPlayer {
     private ClassicalMusic classicalMusic;
-    @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic) {
-        this.classicalMusic = classicalMusic;
-    }
+    private RockMusic rockMusic;
 
-    public void playMusic() {
-        System.out.println("Playing: " + classicalMusic.getSong());
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
+    }
+//
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
+
+//    @Autowired
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
+
+    public String playMusic() {
+        return "Playing: " + classicalMusic.getSong();
     }
 
 }
--- src/main/java/com/turchyn/spring/TestSpring.java
+++ src/main/java/com/turchyn/spring/TestSpring.java
@@ -31,8 +31,10 @@ public class TestSpring {
 //        Music music2 = context.getBean("rockMusic",Music.class);
 //        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
 //        musicPlayer2.playMusic();
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
         context.close();
     }
 }
