9c3b3e4b16b65eb29cb8a9aebd821df73f9d5713
oleh-turchyn <47058088+Leggi19@users.noreply.github.com>
2020-08-20 16:10:36 +0300

    testing scope and init-destroy methods

--- src/main/java/com/turchyn/spring/RockMusic.java
+++ src/main/java/com/turchyn/spring/RockMusic.java
@@ -1,7 +1,13 @@
 package com.turchyn.spring;
 
 public class RockMusic implements Music {
+    public void myInit(){
+        System.out.println("doing my init");
+    }
 
+    public void myDestroy(){
+        System.out.println("doing my destroy");
+    }
     @Override
     public String getSong() {
         return "Loosing my Mind";
--- src/main/java/com/turchyn/spring/TestSpring.java
+++ src/main/java/com/turchyn/spring/TestSpring.java
@@ -11,16 +11,19 @@ public class TestSpring {
         //Music music = context.getBean("musicBean",Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer);
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+//        musicPlayer.playMusic();
+//        System.out.println(musicPlayer);
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
+//
+//        MusicPlayer musicPlayer2 = context.getBean("musicList",MusicPlayer.class);
+//        musicPlayer2.playAllMusic();
 
-
-
-        MusicPlayer musicPlayer2 = context.getBean("musicList",MusicPlayer.class);
-        musicPlayer2.playAllMusic();
+        RockMusic rockMusic = context.getBean("musicBean1",RockMusic.class);
+        System.out.println(rockMusic.getSong());
+        RockMusic rockMusic2 = context.getBean("musicBean1",RockMusic.class);
+        System.out.println(rockMusic2.getSong());
         context.close();
     }
 }
